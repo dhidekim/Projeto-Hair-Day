a9f2fb487572a37ea3e53fc7a19055290aee9116
Douglas <dhidekim@gmail.com>
2024-08-22 14:46:00 -0300

    Buscando os agendamentos na API

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,10 +1,16 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 import { hoursLoad } from "../form/hours-load.js";
 
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
   //Obtém a data do input
   const date = selectedDate.value;
+
+  // Buscar na API os agendamentos
+  const dailySchedules = await scheduleFetchByDay({ date });
+  console.log(dailySchedules);
+
   // Renderiza as horas disponíveis
   hoursLoad({ date });
 }
--- src/services/schedule-fetch-by-day.js
+++ src/services/schedule-fetch-by-day.js
@@ -10,9 +10,10 @@ export async function scheduleFetchByDay({ date }) {
     const data = await response.json();
 
     // Filtra os agendamentos pelo dia selecionado.
-    const dailySchedules = data.filter((schedule) => {
-      dayjs(date).isSame(schedule.when, "day");
-    });
+    const dailySchedules = data.filter((schedule) =>
+      dayjs(date).isSame(schedule.when, "day")
+    );
+    console.log(dailySchedules);
 
     return dailySchedules;
   } catch (error) {

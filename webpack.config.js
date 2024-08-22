30ef790af446f9ca097bc7e79d910eed096fb7d0
Douglas <dhidekim@gmail.com>
2024-08-22 11:42:01 -0300

    Instalando e configurando o Babel com Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,18 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
+        exclude: /node_modules/,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
     ],
   },

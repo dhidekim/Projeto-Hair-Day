e031f267b4479b36aa5c74e2c5554d9dc558cfb2
Douglas <dhidekim@gmail.com>
2024-08-22 14:34:25 -0300

    Serviço para buscar agendamentos do dia

--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -2,6 +2,7 @@ import { apiConfig } from "./api-config";
 
 export async function scheduleNew({ id, name, when }) {
   try {
+    //Faz a requisição para enviar os dados do agendamento.
     await fetch(`${apiConfig.baseURL}/schedules`, {
       method: "POST",
       headers: {
@@ -9,6 +10,8 @@ export async function scheduleNew({ id, name, when }) {
       },
       body: JSON.stringify({ id, name, when }),
     });
+
+    //Exibe mensagem de agendamento realizado com sucesso.
 
     alert("Agendamento realizado com sucesso!");
   } catch (error) {
